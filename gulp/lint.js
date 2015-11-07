f407196eea7b1ea7f7f0f8e8d676c926be038cbb
Devon Bellizio <jdbellizio@gmail.com>
2015-11-07 18:19:00 -0500

    add task to lint css

--- gulp/lint.js
+++ gulp/lint.js
@@ -1,10 +1,21 @@
 'use strict';
 
 module.exports = function(gulp, $, paths) {
+  // lint all the things
+  gulp.task('lint', ['lint:js', 'lint:css']);
+
   // lint js
   gulp.task('lint:js', function() {
     return gulp.src(paths.src.js)
       .pipe($.jshint())
       .pipe($.jshint.reporter('jshint-stylish'));
+  });
+
+  // lint css
+  gulp.task('lint:css', function() {
+    return gulp.src(paths.src.css)
+      .pipe($.sassLint())
+      .pipe($.sassLint.format())
+      .pipe($.sassLint.failOnError());
   });
 };
