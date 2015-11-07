1d557329a300206439237407aaf6558bd99d671b
Devon Bellizio <jdbellizio@gmail.com>
2015-11-07 16:33:06 -0500

    run clean task for each respective build task

--- gulp/build.js
+++ gulp/build.js
@@ -2,13 +2,13 @@
 
 module.exports = function(gulp, $, paths) {
   // build app for dev
-  gulp.task('build:dev', ['clean'], function(cb) {
+  gulp.task('build:dev', ['clean:dev'], function(cb) {
     global.prod = false;
     $.runSequence(['js:dev', 'css', 'img', 'fonts'], 'inject:dev', cb);
   });
 
   // build app for prod
-  gulp.task('build:prod', ['clean'], function(cb) {
+  gulp.task('build:prod', ['clean:prod'], function(cb) {
     global.prod = true;
     $.runSequence(['js:prod', 'css', 'img', 'fonts'], 'inject:prod', 'html:prod', cb);
   });
