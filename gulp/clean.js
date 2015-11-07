4a55801e276ef4b4b0095478b1d8c2d355c880cc
Devon Bellizio <jdbellizio@gmail.com>
2015-11-07 15:40:45 -0500

    add return statement to fix run-sequence issue

--- gulp/clean.js
+++ gulp/clean.js
@@ -5,12 +5,12 @@ module.exports = function(gulp, $, paths) {
   gulp.task('clean', ['clean:dev', 'clean:prod']);
 
   // clean dev build directory
-  gulp.task('clean:dev', function(cb) {
-    $.del([paths.dev.root], cb);
+  gulp.task('clean:dev', function() {
+    return $.del(paths.dev.root);
   });
 
   // clean prod build directory
-  gulp.task('clean:prod', function(cb) {
-    $.del([paths.prod.root], cb);
+  gulp.task('clean:prod', function() {
+    return $.del(paths.prod.root);
   });
 };
