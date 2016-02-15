010cedf8abf3ef7c61b7c9b13d9fa55dd26e246f
Devon Bellizio <jdbellizio@gmail.com>
2016-02-14 19:01:01 -0500

    log size of html

--- gulp/html.js
+++ gulp/html.js
@@ -11,6 +11,7 @@ module.exports = function(gulp, $, paths, env) {
 
     return gulp.src(paths.prod.root + '/index.html')
       .pipe($.minifyHtml(options))
+      .pipe($.size({title: 'HTML'}))
       .pipe(gulp.dest(paths.prod.root));
   });
 };
