8077f4a668df3d56b72bf2512ccff56875150cc5
Devon Bellizio <jdbellizio@gmail.com>
2016-02-16 14:37:00 -0500

    swap gulp-minify-html for gulp-htmlmin

--- gulp/html.js
+++ gulp/html.js
@@ -4,13 +4,12 @@ module.exports = function(gulp, $, paths, env) {
   // minify html for prod
   gulp.task('html:prod', function() {
     var options = {
-      empty: true,
-      spare: true,
-      quotes: true
+      removeComments: true,
+      collapseWhitespace: true
     };
 
     return gulp.src(paths.prod.root + '/index.html')
-      .pipe($.minifyHtml(options))
+      .pipe($.htmlmin(options))
       .pipe($.size({title: 'HTML'}))
       .pipe(gulp.dest(paths.prod.root));
   });
